7925b5b1ec4b45ac4470685eab5c0e1df77d84b7
Takashiidobe <idobetakashi@gmail.com>
2026-07-11 06:50:16 -0400

    external definitions

---
+++ tests/fixtures/extern_decl.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int toupper(int c);
+int tolower(int c);
+int abs(int n);
+
+int main(void) {
+    printf("%c%c %d\n", toupper('a'), tolower('Z'), abs(-42));
+    return 0;
+}
