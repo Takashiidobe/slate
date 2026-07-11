72c3c218d8fcd9bee47376e1c39d53e8a7393614
Takashiidobe <idobetakashi@gmail.com>
2026-07-11 07:33:15 -0400

    Add stdio coverage probes

---
+++ tests/stdlib/stdio/perror_missing.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    FILE *f = fopen("slate_stdio_missing_input.tmp", "r");
+    if (!f) {
+        perror("stdio-probe");
+        return 0;
+    }
+    fclose(f);
+    return 0;
+}
