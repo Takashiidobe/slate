ebf67c582e67aa3609a00705b28b44c3a812492a
Takashiidobe <idobetakashi@gmail.com>
2026-07-11 07:33:15 -0400

    Add stdio coverage probes

---
+++ tests/stdlib/stdio/tmpfile_fscanf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    FILE *f = tmpfile();
+    fputs("12 xy 4.5", f);
+    rewind(f);
+    int i = 0;
+    char word[3] = {0};
+    double d = 0.0;
+    int n = fscanf(f, "%d %2s %lf", &i, word, &d);
+    printf("%d %d %s %.1f\n", n, i, word, d);
+    fclose(f);
+    return 0;
+}
