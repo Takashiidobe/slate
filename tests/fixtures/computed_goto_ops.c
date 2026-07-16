720c77d5b5e0942a67a1cda3f6e0de644c7b0dc7
Takashiidobe <idobetakashi@gmail.com>
2026-07-15 21:10:40 -0400

    Lower inline asm and computed goto CIR ops

---
+++ tests/fixtures/computed_goto_ops.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+static int jump_probe(int x) {
+    static void *targets[] = {&&zero, &&one, &&two};
+    if (x < 0 || x > 2) {
+        return -1;
+    }
+    goto *targets[x];
+
+zero:
+    return 10;
+one:
+    return 20;
+two:
+    return 30;
+}
+
+int main(void) {
+    volatile int input = 2;
+    printf("%d\n", jump_probe(input));
+    return 0;
+}
