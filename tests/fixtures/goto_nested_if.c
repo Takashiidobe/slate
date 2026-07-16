5daf5717b3525608ee638dd758a96dd14f45e7eb
Takashiidobe <idobetakashi@gmail.com>
2026-07-16 08:43:39 -0400

    Structure additional reducible goto fixtures

---
+++ tests/fixtures/goto_nested_if.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int a = 3;
+    int b = -1;
+    int r;
+    if (a > 0) {
+        if (b > 0)
+            goto both_pos;
+        else
+            goto a_pos;
+    } else {
+        goto other;
+    }
+both_pos:
+    r = 1;
+    goto done;
+a_pos:
+    r = 2;
+    goto done;
+other:
+    r = 3;
+    goto done;
+done:
+    printf("%d\n", r);
+    return 0;
+}
