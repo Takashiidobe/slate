4b78982bb628bd5ae5466df1f8166a78c3e00173
Takashiidobe <idobetakashi@gmail.com>
2026-07-15 21:10:40 -0400

    Lower inline asm and computed goto CIR ops

---
+++ tests/fixtures/inline_asm_ops.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+static int asm_probe(int x) {
+    __asm__ volatile("" : "+r"(x));
+    return x + 1;
+}
+
+int main(void) {
+    volatile int input = 4;
+    printf("%d\n", asm_probe(input));
+    return 0;
+}
