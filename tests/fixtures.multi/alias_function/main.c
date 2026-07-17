66c6d14003ad4c00d7df5221fa02a2ef4031a1ed
Takashiidobe <idobetakashi@gmail.com>
2026-07-16 20:53:58 -0400

    Lower function alias attributes

---
+++ tests/fixtures.multi/alias_function/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int real_impl(int x) {
+    return x * 3 + 1;
+}
+
+extern int alias_impl(int x) __attribute__((alias("real_impl")));
+
+int main(void) {
+    printf("%d %d\n", alias_impl(5), real_impl(2));
+    return 0;
+}
---
+++ tests/fixtures.unsupported/alias_global.c
@@ -0,0 +1,6 @@
+int real_global = 12;
+extern int alias_global __attribute__((alias("real_global")));
+
+int main(void) {
+    return alias_global;
+}
