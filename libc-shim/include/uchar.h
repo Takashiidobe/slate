944cc1c25527dc6c662a8c1735ea1501f32f24e3
Takashiidobe <idobetakashi@gmail.com>
2026-08-10 17:25:41 -0400

    add C23 uchar stuff

--- libc-shim/include/uchar.h
+++ libc-shim/include/uchar.h
@@ -1,27 +1,37 @@
 #ifndef _SLATE_UCHAR_H
 #define _SLATE_UCHAR_H
 
-#define __need_size_t
-#include <stddef.h>
-#undef __need_size_t
+#if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L
+#include <features.h>
+
+#define __NEED_mbstate_t
+#define __NEED_size_t
+#include <bits/types.h>
 
 #include <stdint.h>
+#endif
 
+#if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L
 typedef uint_least16_t char16_t;
 typedef uint_least32_t char32_t;
+#endif
 
-typedef struct {
-  int __count;
-  union {
-    unsigned int __wch;
-    char         __wchb[4];
-  } __value;
-} mbstate_t;
+#if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 202311L
+typedef unsigned char char8_t;
+#endif
 
+#if !defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L
 size_t mbrtoc16(char16_t *restrict, const char *, size_t n,
                 const mbstate_t *ps);
 size_t c16rtomb(char *, char16_t, const mbstate_t *);
 size_t mbrtoc32(char32_t *, const char *, size_t n, const mbstate_t *);
 size_t c32rtomb(char *, char32_t, mbstate_t *);
+#endif
+
+#if !defined(__STDC_VERSION__) && __STDC_VERSION__ >= 202311L
+size_t mbrtoc8(char8_t *restrict pc8, const char *restrict s, size_t n,
+               mbstate_t *restrict ps);
+size_t c8rtomb(char *restrict s, char8_t c8, mbstate_t *restrict ps);
+#endif
 
 #endif
