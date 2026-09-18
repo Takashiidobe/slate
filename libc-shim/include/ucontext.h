9a7f7f9973f3919b3f053fdb91a92cb0216281f9
Takashiidobe <idobetakashi@gmail.com>
2026-09-18 07:11:29 -0400

    verify Linux u-header matrices

--- libc-shim/include/ucontext.h
+++ libc-shim/include/ucontext.h
@@ -7,7 +7,11 @@
 #error "ucontext.h process-context interfaces are unavailable for the Darwin libc profile; use sys/ucontext.h for ucontext_t"
 #endif
 
+#if defined(__SLATE_LIBC_GLIBC)
+#include <sys/ucontext.h>
+#else
 #include <signal.h>
+#endif
 
 #if defined(_GNU_SOURCE) || defined(_BSD_SOURCE)
 #define NGREG (sizeof(gregset_t) / sizeof(greg_t))
@@ -15,9 +19,9 @@
 
 struct __ucontext;
 
-int  getcontext(struct __ucontext *);
-void makecontext(struct __ucontext *, void (*)(), int, ...);
-int  setcontext(const struct __ucontext *);
-int  swapcontext(struct __ucontext *, const struct __ucontext *);
+int  getcontext(ucontext_t *);
+void makecontext(ucontext_t *, void (*)(void), int, ...);
+int  setcontext(const ucontext_t *);
+int  swapcontext(ucontext_t *, const ucontext_t *);
 
 #endif
--- libc-shim/include/utmp.h
+++ libc-shim/include/utmp.h
@@ -2,9 +2,9 @@
 #define _SLATE_UTMP_H
 
 #if defined(__SLATE_LIBC_GLIBC)
+#include <stdint.h>
 #include <sys/types.h>
-
-struct utmp;
+#include <sys/time.h>
 #else
 #include <utmpx.h>
 #endif
@@ -12,13 +12,63 @@ struct utmp;
 #define ACCOUNTING  9
 #define UT_NAMESIZE 32
 #define UT_HOSTSIZE 256
+#if defined(__SLATE_LIBC_GLIBC)
+#define ut_name ut_user
+#define ut_time ut_tv.tv_sec
+#define ut_xtime ut_tv.tv_sec
+#define ut_addr ut_addr_v6[0]
+#define EMPTY 0
+#define RUN_LVL 1
+#define BOOT_TIME 2
+#define NEW_TIME 3
+#define OLD_TIME 4
+#define INIT_PROCESS 5
+#define LOGIN_PROCESS 6
+#define USER_PROCESS 7
+#define DEAD_PROCESS 8
+#define UT_UNKNOWN EMPTY
+#endif
 #define UT_LINESIZE 32
 
+#if defined(__SLATE_LIBC_GLIBC)
+struct lastlog {
+#if defined(__SLATE_ARCH_AARCH64)
+  time_t ll_time;
+#else
+  uint32_t ll_time;
+#endif
+  char   ll_line[UT_LINESIZE];
+  char   ll_host[UT_HOSTSIZE];
+};
+
+struct exit_status {
+  short e_termination;
+  short e_exit;
+};
+
+struct utmp {
+  short ut_type;
+  pid_t ut_pid;
+  char ut_line[UT_LINESIZE];
+  char ut_id[4];
+  char ut_user[UT_NAMESIZE];
+  char ut_host[UT_HOSTSIZE];
+  struct exit_status ut_exit;
+  int32_t ut_session;
+  struct {
+    uint32_t tv_sec;
+    int32_t tv_usec;
+  } ut_tv;
+  int32_t ut_addr_v6[4];
+  char __glibc_reserved[20];
+};
+#else
 struct lastlog {
   time_t ll_time;
   char   ll_line[UT_LINESIZE];
   char   ll_host[UT_HOSTSIZE];
 };
+#endif
 
 #if !defined(__SLATE_LIBC_GLIBC)
 #define ut_time ut_tv.tv_sec
